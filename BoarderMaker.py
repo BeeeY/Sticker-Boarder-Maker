249e34c69d8e2245c9f0e652d397a45b3f134e28
BeeeY <beilinyao544@gmail.com>
2026-09-23 20:56:42 -0500

    made a tkinter interface

--- BoarderMaker.py
+++ BoarderMaker.py
@@ -1,25 +1,43 @@
 from PIL import Image
 from PIL import ImageFilter
+from PIL import ImageTk
+import tkinter as Tk
+
+root = Tk.Tk()
+root.title("Sticker Border Maker")
+
+
+StickerPath = Tk.Label(root, Text="Sticker Path: ")
+StickerPath.pack()
+PathButton = Tk.Button(root, command=filedialog.askopenfilename())
+BoarderSize = Tk.Label(root, Text="Boarder Size: ")
+BoarderSize.pack()
+BoarderScale = Tk.Scale(root,from_=1, to=100, orient="horizontal")
+BoarderScale.pack()
+Generate = Tk.Button(root, command=boardermaker())
+Generate.pack()
 
 path = input("Put image path (no quotes): ")
 thickness = int(input("Put border Size (pxls): "))
 
-img = Image.open(path)
-img = img.convert("RGBA")
+def boardermaker(img, thickness):
 
-w, h = img.size
-canvas = Image.new("RGBA", (w + 2*thickness, h + 2*thickness), (0, 0, 0, 0))
-canvas.paste(img, (thickness, thickness))
+    img = Image.open(path)
+    img = img.convert("RGBA")
 
-mask = canvas.getchannel("A")
+    w, h = img.size
+    canvas = Image.new("RGBA", (w + 2*thickness, h + 2*thickness), (0, 0, 0, 0))
+    canvas.paste(img, (thickness, thickness))
 
-mask = mask.filter(ImageFilter.MaxFilter(thickness*2+1))
+    mask = canvas.getchannel("A")
 
-border = Image.new("RGBA", canvas.size, (255, 255, 255, 255))
-border.putalpha(mask)
+    mask = mask.filter(ImageFilter.MaxFilter(thickness*2+1))
 
-sticker = Image.alpha_composite(border, canvas)
+    border = Image.new("RGBA", canvas.size, (255, 255, 255, 255))
+    border.putalpha(mask)
 
-sticker.save("sticker.png")
-print("done")
+    sticker = Image.alpha_composite(border, canvas)
 
+    sticker.save("sticker.png")
+
+root.mainloop()
