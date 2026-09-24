7baadc58eb11db943a37572cede76a22d2ce4467
BeeeY <beilinyao544@gmail.com>
2026-09-23 20:21:01 -0500

    finished boarder maker

--- BoarderMaker.py
+++ BoarderMaker.py
@@ -1,7 +1,25 @@
 from PIL import Image
 from PIL import ImageFilter
-from PIL import ImageChops
+
+path = input("Put image path (no quotes): ")
+thickness = int(input("Put border Size (pxls): "))
 
 img = Image.open(path)
 img = img.convert("RGBA")
-print(.img.mode)
+
+w, h = img.size
+canvas = Image.new("RGBA", (w + 2*thickness, h + 2*thickness), (0, 0, 0, 0))
+canvas.paste(img, (thickness, thickness))
+
+mask = canvas.getchannel("A")
+
+mask = mask.filter(ImageFilter.MaxFilter(thickness*2+1))
+
+border = Image.new("RGBA", canvas.size, (255, 255, 255, 255))
+border.putalpha(mask)
+
+sticker = Image.alpha_composite(border, canvas)
+
+sticker.save("sticker.png")
+print("done")
+
